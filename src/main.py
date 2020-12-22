40c27b2152048b47cd0f01e9ce78f0c523e812c2
SBOne-Kenobi <snob-1432e@mail.ru>
2020-12-22 18:13:03 +0500

    fix main

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     print("Start tests:")
     run_all_tests()
     print("------------------")
-    # print("Start tasks:")
-    # run_all_tasks()
+    print("Start tasks:")
+    run_all_tasks()
 
 
